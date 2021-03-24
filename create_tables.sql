cf65b4579a671afdb92eb6c8d587d975b586a3f2
Divya Sundaresan <divyasundaresan98@gmail.com>
2021-03-24 13:27:12 -0700

    updated reviewers and review tables

--- create_tables.sql
+++ create_tables.sql
@@ -1,42 +1,63 @@
 
 BEGIN TRANSACTION;
 
---*******************************************************
--- Applicant table
--- use WITHOUT ROWID
--- NO ENUM so should we create a new table for those probably unnecessary?
+/*******************************************************************************
+   Drop Tables
+********************************************************************************/
 
 DROP TABLE IF EXISTS Applicant;
+DROP TABLE IF EXISTS Reviewer;
+DROP TABLE IF EXISTS Review;
+/*******************************************************************************
+   Create Tables
+********************************************************************************/
+
+
 CREATE TABLE Applicant (
-  ApplicantID INT PRIMARY KEY,
-  Lname TEXT NOT NULL,
-  Fname TEXT NOT NULL,
-  email TEXT NOT NULL UNIQUE,
-  state TEXT,
-  zip INT,
-  city TEXT,
-  country TEXT,
-  straddress TEXT,
-  phone TEXT NOT NULL UNIQUE,
-  dob TEXT,
-  gender TEXT,
-  citizen/resident TEXT,
-  firstgen INT,
-  vet INT,
-  Race TEXT,
-  hispanic INT,
-  revstatus TEXT,
-  submit_date TEXT,
-  gradschool TEXT,
-  gradschool YEAR INT
-  major TEXT,
-  gpa REAL,
-  Applicant_info BLOB
+  aid integer not null auto_increment primary key,
+	fname text,
+	lname text,
+	email text,
+	state text,
+	city text,
+	country text,
+	dob text,
+	gender text,
+	citizen text,
+	firstgen text,
+	veteran text,
+	hispanic text,
+	gradyear integer,
+	race text,
+	housing text,
+	institution text,
+	standing text,
+	major text,
+	gpa float,
+	comcollege text,
+	compexperience text,
+	referenced text,
+	reviewstatus text,
+	submitdate text,
+	documents blob
 ) WITHOUT ROWID;
 
---*******************************************************
+
+create table Reviewer (
+  rid integer not null auto_increment primary key,
+  lname text,
+  fname text
+) WITHOUT ROWID;
 
 
+
+create table Review (
+  aid integer,
+	rid integer,
+	primary key(aid, rid),
+	foreign key(aid) references Applicant(aid),
+	foreign key(rid) references Reviewer(rid)
+) WITHOUT ROWID;
 --*******************************************************
 
 COMMIT;
