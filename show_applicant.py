5c10a44810cba7da4a11cfb05d615cf5e0374c93
Janvee-Patel <78512468+Janvee-Patel@users.noreply.github.com>
2021-03-25 22:03:14 -0500

    Added python file to show applicant data
    This file is a rough template of showing the select fields from the applicant db. I have tried multiple ways to configure the cgi-bin but have not had any success.

---
+++ show_applicant.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+#also I don't see any use of including cgi here because all this script is doing is showing data
+import sys
+import cgi
+import cgitb
+import sqlite3
+
+cgitb.enable()
+
+#start html
+print("Content-type: text/html\n")
+print("<html><head>")
+print("<title>BRITE REU Applicants</title>")
+print("<body>")
+print("<h1>Applicant Information</h1>")
+
+#did not include form action here in this script since user is not inputting anything yet
+
+#start table
+print("<table id=Applicant>")
+#print headers for html table
+print("<tr><th>Applicant ID</th><th>First Name</th><th>Last Name</th></tr>")
+
+#query to print applicant data
+query1 = "SELECT aid, firstname, lastname FROM Applicant;"
+
+try:
+    #connect to sqlite database file
+    connection = sqlite3.connect('BRITEREU.db')
+    c = connection.cursor()
+    #execute query
+    c.execute(query)
+    #get results to above standard query
+    results = c.fetchall()
+    
+    #print results of query to html table
+    for row in results:
+        print("<tr><td>%s</td><td>%s</td><td>%s</td></tr>" % (row[0], row[1], row[2]))
+    cursor.close()
+    connection.close()
+
+#query does not execute properly
+except Exception:
+    print("<p><font color=red><b>Error</b></p>")
+
+print("</table>")
+print("</body></html>")
