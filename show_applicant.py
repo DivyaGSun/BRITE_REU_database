8d81be81b86f7bb01e3e1e9397bfda786d6fd406
Divya Sundaresan <divyasundaresan98@gmail.com>
2021-03-31 12:46:51 -0700

    added filtered table, html table formatting, file runs on unix

--- show_applicant.py
+++ show_applicant.py
@@ -1,54 +1,95 @@
 #!/usr/bin/python
 
-#this script is a draft to only show the applicant table
-#it will be expanded to include more functionality such as selecting the applicant to go to their applicant page, etc. 
-
 import sys
 import cgi
 import cgitb
 import sqlite3
 reload(sys)
+#import sys
 sys.setdefaultencoding('utf-8')
+
 cgitb.enable()
 
-#start html
+# html
 print("Content-type: text/html\n")
+print('<meta charset="utf-8">')
 print("<html><head>")
 print("<title>BRITE REU Applicants</title>")
+#print('''<link rel="stylesheet" href="/home/assets/css/bootstrap.min.css>''')
+print('''<style>
+body {margin:30;padding:30}
+#Applicant {
+        font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+        border-collapse: collapse;
+        width: 50%;
+}
+
+#Applicant td, #Applicant th {
+        border: 1px solid #ddd;
+        padding: 8px;
+}
+
+#Applicant tr:nth-child(even){background-color: #f2f2f2;}
+
+#Applicant tr:hover {background-color: #ddd;}
+
+#Applicant th {
+        padding-top: 12px;
+        padding-bottom: 12px;
+        text-align: left;
+        background-color: #5C30B2;
+        color: white;
+}
+</style>
+</head>''')
 print("<body>")
 print("<h1>Applicant Information</h1>")
-
-#did not include form action here in this script since user is not inputting anything yet
-#will expand to include when user inputs information such as selection of applicant
-
-#start table
-print("<table id=Applicant>")
-#print headers for html table
+#did not include form action right now
+#print('''<form action="https://bioed.bu.edu/cgi-bin/students_21/jpatel2/show_applicant.py" method="post" >
+# </form>''')
+print('''<table id= Applicant class="dataframe">''')
 print("<tr><th>Applicant ID</th><th>First Name</th><th>Last Name</th><th>Email Address</th><th>Date Submitted</th><th>Review Status</th></tr>")
 
 #query to print applicant data
 query1 = "SELECT aid, firstname, lastname, emailaddress, submitdate, reviewstatus FROM Applicant;"
 
-try:
-    #connect to sqlite database file
-    connection = sqlite3.connect('BRITEREU.db')
-    c = connection.cursor()
-    try:
-        #execute query
-        c.execute(query1)
-        #get results to above standard query
-        results = c.fetchall()
-    except Exception:
-        print("<p><font color=red><b>Error</b>while executing query</p>")
-    #print results of query to html table
-    for row in results:
-        print("<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>" % (row[0], row[1], row[2], row[3], row[4], row[5]))
-    c.close()
-    connection.close()
 
-#there was an error with printing the results
+connection = sqlite3.connect('BRITEREU.db')
+c = connection.cursor()
+try:
+    #execute query
+    c.execute(query1)
+    #get results to above standard query
+    results = c.fetchall()
+
 except Exception:
-    print("<p><font color=red><b>Error</b></p>")
+    print("<p><font> color=red><b>Error</b></font></p>")
+
+for row in results:
+    print("<tr><td>%s</td><td>%s</td><td>%s</td>><td>%s</td><td>%s</td><td>%s</td></tr>" % (row[0], row[1], row[2], row[3], row[4], row[5]))
+
+c.close()
+connection.close()
 
 print("</table>")
-print("</body></html>")
+#print("</body>")
+print('''<script src="https://bioed.bu.edu/students_21/divyas3/tablefilter/tablefilter.js"></script>''')
+print('''<script data-config="">
+ var filtersConfig = {
+     base_path: 'https://bioed.bu.edu/students_21/divyas3/tablefilter/',
+  auto_filter: {
+                    delay: 110 //milliseconds
+              },
+              filters_row_index: 1,
+              state: true,
+              alternate_rows: true,
+              rows_counter: true,
+              btn_reset: true,
+              status_bar: true,
+              msg_filter: 'Filtering...'
+            };
+            var tf = new TableFilter(Applicant, filtersConfig);
+            tf.init();
+          </script>''')
+print("</body> </html>")
+#print("</html>")
