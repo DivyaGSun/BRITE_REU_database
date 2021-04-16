68ec7511064ed3045fa0a65e2f0c7325d6897988
Divya Sundaresan <divyasundaresan98@gmail.com>
2021-04-15 18:59:45 -0700

    inserting review works

--- reviewer.py
+++ reviewer.py
@@ -4,7 +4,8 @@ import cgi
 import cgitb
 import sqlite3
 reload(sys)
-#import sys
+import traceback
+
 sys.setdefaultencoding('utf-8')
 
 cgitb.enable()
@@ -50,25 +51,42 @@ textarea {display:block;}
 print("<body>")
 print("<h1>Submit a Review</h1>")
 
-print('''<form name="myForm" form action="https://bioed.bu.edu/cgi-bin/students_21/divyas3/reviewer.py" method="post" >
-		<input type ='hidden' id='ID' name="AID" >
-
-        <label for="Review">Enter review of Applicant:</label>
-        <textarea id="Review" name="Review" rows="20" cols="80" ></textarea>
-        <input type="submit" value="Submit">
-		</form>''')
+# print('''<form name="myForm" form action="https://bioed.bu.edu/cgi-bin/students_21/divyas3/reviewer.py" method="post" >
+# 		<input type ='hidden' id='ID' name="AID" value = AID>
+#
+#         <label for="Review">Enter review of Applicant:</label>
+#         <textarea id="Review" name="Review" rows="20" cols="80" ></textarea>
+#         <input type="submit" onclick= "clicked()" value="Submit">
+# 		</form>''')
 
 # get the form
 form = cgi.FieldStorage()
 # get three values from the form
-AID = form.getvalue('AID')
+AID = str(form.getvalue('AID'))
 Rev = form.getvalue('Review')
+UID = str(0)
+AIDs = str(AID)
+
+#
+print('''<form name="myForm" form action="https://bioed.bu.edu/cgi-bin/students_21/group_proj/group_K/reviewer.py" method="GET" >
+		<input type ='hidden' id='ID' name="AID" value = %s>
+
+        <label for="Review">Enter review of Applicant:</label>
+        <textarea id="Review" name="Review" rows="20" cols="80" ></textarea>
+        <input type="submit" onclick= "clicked()" value="Submit">
+		</form>'''%(AID))
+
+# AID = str(form.getvalue('AID'))
+# Rev = str(form.getvalue('Review'))
+# UID = str(0)
+# AIDs = str(AID)
 
 query1 = """SELECT aid, firstname, lastname, emailaddress, submitdate, reviewstatus
 FROM Applicant
 WHERE aid='%s';""" %(AID)
 
-connection = sqlite3.connect('BRITEREU.db')
+#connection = sqlite3.connect('/https://bioed.bu.edu/students_21/divyas3/BRITEREU.db')
+connection = sqlite3.connect('db/BRITEREU.db')
 c = connection.cursor()
 try:
     #execute query
@@ -86,13 +104,51 @@ print("<tr><th>Applicant ID</th><th>First Name</th><th>Last Name</th><th>Email A
 for row in results:
     print('''<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>''' % (row[0], row[1], row[2], row[3], row[4], row[5]))
 
-#if Rev:
-    #'''query = UPDATE reviews set review = %s where aid =%s'''(%Rev, %AID)
-    #print("<h2>Thanks your Review for Applicant Id %s have been recorded!</h2>" %AID)
+print("</table>")
+if Rev:
+    print('true')
+if Rev:
+    print(AIDs)
+    insert = "INSERT INTO Review (aid,uid,reviews) VALUES (?,?,?)"
+    print(str(insert))
+    try:
+        c.execute(insert, (AIDs, UID , Rev))
+        connection.commit()
+    #except Exception as sqlError:
+       # print(sqlError)
+	#print(str(AID))
+        #print(str(UID))
+        #print("|%s|"%(Rev))
+    except sqlite3.Error as er:
+    	print('SQLite error: %s' % (' '.join(er.args)))
+    	print("Exception class is: ", er.__class__)
+    	print('SQLite traceback: ')
+    	exc_type, exc_value, exc_tb = sys.exc_info()
+    	print(traceback.format_exception(exc_type, exc_value, exc_tb))
+        print(str(AID))
+        print(str(UID))
+        print("|%s|"%(Rev))
+    # prnt("<h2>Thanks your Review for Applicant Id %s have been recorded!</h2>" %AIDs)
+    # query2 = "UPDATE Applicant set reviewstatus = Completed"
+    # c.execute(query)
+
+
 c.close()
 connection.close()
-print("</table>")
+#print("</table>")
+
 print('Link to download full application:---(hyperlink this)')
 #add reviewers past information
 print("<h3>Previous Reviews Completed</h3>")
+
+print('''<script type="text/javascript">
+    function clicked() {
+       if (confirm('Do you want to submit?')) {
+           submit.submit();
+       } else {
+           return false;
+       }
+    }
+
+</script>''')
 print("</body></html>")
